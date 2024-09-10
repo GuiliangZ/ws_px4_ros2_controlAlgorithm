4329ae1857c153f55e51b3f08ea9ffefad947dad
Guiliang-Linux <guiliangzheng@outlook.com>
2024-09-10 18:46:21 +0000

    First build of the ros2 workspace with px4-msgs and px4-com built in

---
+++ build/px4_msgs/px4_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/guiliang-linux/ws_px4_ros2_controlAlgorithm/build/px4_msgs/px4_msgs__py
+# Build directory: /home/guiliang-linux/ws_px4_ros2_controlAlgorithm/build/px4_msgs/px4_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
