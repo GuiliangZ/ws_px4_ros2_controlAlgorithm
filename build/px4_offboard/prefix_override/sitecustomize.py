3ec7935aee502a330d47fa26e88788b867d262cf
smartadi <josh.holder72@gmail.com>
2024-09-15 09:30:29 -0700

    modify the velocity_position setup file

--- build/px4_offboard/prefix_override/sitecustomize.py
+++ build/px4_offboard/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/guiliang-linux/Documents/projects/DroneProject/ws_px4_ros2_controlAlgorithm/install/px4_offboard'
+    sys.prefix = sys.exec_prefix = '/home/mist/Documents/projects/DroneProject/ws_px4_ros2_controlAlgorithm/install/px4_offboard'
--- src/px4_offboard_velocity/px4_offboard/setup.py
+++ src/px4_offboard_velocity/px4_offboard/setup.py
@@ -31,7 +31,8 @@ setup(
                 'visualizer = px4_offboard.visualizer:main',
                 'velocity_control = px4_offboard.velocity_control:main',
                 'control = px4_offboard.control:main',
-                'processes = px4_offboard.processes:main'
+                'processes = px4_offboard.processes:main',
+                'position_velocity = px4_offboard.position_velocity_control:main',
         ],
     },
 )
